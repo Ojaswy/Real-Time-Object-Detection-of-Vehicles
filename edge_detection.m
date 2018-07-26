6498416fe6ea8ff8dd33479d74d1b4d63445f429
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2018-07-26 21:57:30 +0530

    Update edge_detection.m

--- edge_detection.m
+++ edge_detection.m
@@ -1,12 +1,6 @@
 RGB = imread('C:\Users\Desktop\Vehicle Detection\cars.jpg');
 imshow(RGB);
 I = rgb2gray(RGB);
-% figure
-% imshow(I)
 [Gmag, Gdir] = imgradient(I,'prewitt');
-% figure
-% imshowpair(Gmag, Gdir, 'montage');
-% title('Gradient Magnitude, Gmag (left), and Gradient Direction, Gdir (right), using Prewitt method')
-
 figure, imshow(Gmag, []), title('Gradient magnitude')
 figure, imshow(Gdir, []), title('Gradient direction')
