3e3abf4df2f6c718efd569022de05d4e97976c0a
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2018-07-26 21:58:26 +0530

    Update hough.py

--- hough.py
+++ hough.py
@@ -28,13 +28,12 @@ cv2.fillPoly(mask, vertices, ignore_mask_color)
 masked_edges = cv2.bitwise_and(edges, mask)
 
 # Define the Hough transform parameters
-# Make a blank the same size as our image to draw on
-rho = 2 # distance resolution in pixels of the Hough grid
-theta = math.pi/180 # angular resolution in radians of the Hough grid
-threshold = 15     # minimum number of votes (intersections in Hough grid cell)
-min_line_length = 40 #minimum number of pixels making up a line
-max_line_gap = 20    # maximum gap in pixels between connectable line segments
-line_image = np.copy(image)*0 # creating a blank to draw lines on
+rho = 2 
+theta = math.pi/180
+threshold = 15    
+min_line_length = 40 
+max_line_gap = 20    
+line_image = np.copy(image)*0 
 
 # Run Hough on edge detected image
 # Output "lines" is an array containing endpoints of detected line segments
