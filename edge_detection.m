357a8418d5c8a418bc0e18e1896ce2d1c153812e
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2018-07-28 22:15:24 +0530

    Update edge_detection.m

--- edge_detection.m
+++ edge_detection.m
@@ -1,4 +1,4 @@
-RGB = imread('C:/Users/Dell/Desktop/Vehicle Detection/cars.jpg');
+RGB = imread('C:/Users/Dell/Desktop/Vehicle Detection/test.jpg');
 imshow(RGB);
 I = rgb2gray(RGB);
 [Gmag, Gdir] = imgradient(I,'prewitt');
