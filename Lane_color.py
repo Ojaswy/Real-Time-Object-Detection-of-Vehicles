0423b17c59c09b96be0f0a7af868f928648814be
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2018-08-23 22:22:18 +0530

    Update Lane_color.py

--- Lane_color.py
+++ Lane_color.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
+# Reading the image
 image = mpimg.imread('test.jpg')
 
 # Grab the x and y size and make a copy of the image
